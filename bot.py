61735a49f034d9d9ee216dacbe0654e5a0784afd
alon gouldman <alongouldman@gmail.com>
2018-11-02 06:29:28 +0200

    update files

--- bot.py
+++ bot.py
@@ -1,4 +1,4 @@
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+# from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import telebot
 import time
 import re
