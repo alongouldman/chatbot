64550c847d1df4b39e09b6f1c80c6b6df533770e
alon gouldman <alongouldman@gmail.com>
2018-12-19 19:38:32 +0200

    diffarent worksheets every month

--- custom_errors.py
+++ custom_errors.py
@@ -6,8 +6,6 @@ class NoCategoryError(Exception):
     pass
 
 
-def unknown_error(err, bot, message):
-    if hasattr(err, 'message'):
-        bot.reply_to(message, err.message)
-    else:
-        bot.reply_to(message, err)
+class NoRowsError(Exception):
+    pass
+
