71db55c671a1f13fe4fcda893a59f3afd1f8dd10
alon.g <alon.g@claroty.com>
2020-01-29 11:11:16 +0200

    try diffarent procfile

--- web/hello.py
+++ web/hello.py
@@ -6,3 +6,8 @@ logger = logging.getLogger(__name__)
 print("hello")
 logging.info("hello logs")
 logger.info("hello logger")
+
+if __name__ == "__main__":
+	print("hello inside main")
+	logging.info("hello logs from main")
+	logger.info("hello logger from main")
