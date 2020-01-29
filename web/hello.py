8839800026be62a2913f1acdc4942ab76fcd7d2e
alon.g <alon.g@claroty.com>
2020-01-29 10:50:44 +0200

    try to log diffarently

--- web/hello.py
+++ web/hello.py
@@ -1,4 +1,8 @@
 import logging
+# Enable logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 print("hello")
 logging.info("hello logs")
+logger.info("hello logger")
